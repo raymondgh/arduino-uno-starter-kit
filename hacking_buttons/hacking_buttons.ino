7c5e130e5daf0731f0cd5b2956d868d21f42dda4
Ray Harris <raymondgh@gmail.com>
2017-11-04 20:23:59 -0700

    15

---
+++ hacking_buttons/hacking_buttons.ino
@@ -0,0 +1,57 @@
+const int optoPin = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(optoPin, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// morse code for "LOL"
+  digitalWrite(optoPin, HIGH);
+  delay(200);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(500);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(200);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(200);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(500);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(500);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(500);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+    digitalWrite(optoPin, HIGH);
+  delay(200);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(500);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(200);
+  digitalWrite(optoPin, LOW);
+  delay(200);
+  digitalWrite(optoPin, HIGH);
+  delay(200);
+  digitalWrite(optoPin, LOW);
+  delay(1000);
+
+}
